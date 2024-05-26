df8286efad3ff303d73a99c75c7c5939a9479066
doravissar <doravissar2@gmail.com>
2024-05-26 17:46:21 +0300

    tests

--- backend/tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
+++ backend/tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5b2ebd9d289951e32e345f28e678c80811b7a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39c4a0d4d1f47fad27cfe35040fcbc08923e375e")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
