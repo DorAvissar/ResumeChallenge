39c2c885b5b5697bb8644bba14d3aae82e6fb3c3
doravissar <doravissar2@gmail.com>
2024-05-26 23:35:54 +0300

    last try

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -21,12 +21,11 @@ namespace Company.Function
             [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
             ILogger log)
         {
-            // Here is where the counter gets updated! 
+            // Here is where the counter gets updated!!! 
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             updatedCounter = counter;
             updatedCounter.Count += 1;
-
             var jsonToRetun = JsonConvert.SerializeObject(counter);
 
 
